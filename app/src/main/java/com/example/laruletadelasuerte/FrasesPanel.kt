320be75258c336bd32dec900b5821e89c787836c
Claudia <claudia.velasco.delacalle@iesjulianmarias.es>
2025-02-07 18:14:21 +0100

    a

--- app/src/main/java/com/example/laruletadelasuerte/FrasesPanel.kt
+++ app/src/main/java/com/example/laruletadelasuerte/FrasesPanel.kt
@@ -13,25 +13,25 @@ class FrasesPanel {
         ),
 
         2 to listOf(
-            "LIQUIDO QUE BEBEMOS Y PROVIENE DE UN ANIMAL" to "Definición de concepto",
-            "" to "Letra de una canción",
-            "" to "Letra de una canción",
-            "" to "Letra de una canción",
-            "SE GENERA A TRAVES DE BATERIAS" to "Descubrimiento científico"
+            "  LIQUIDO QUE   BEBEMOS Y     PROVIENE DE    UN ANIMAL  " to "Definición de concepto",
+            " ESCOGISTE A   LA MAS GUAPA     Y A LA      MENOS BUENA " to "Letra de una canción",
+            "HOY PUEDE SER  UN GRAN DIA   PLANTEATELO        ASI     " to "Letra de una canción",
+            "   COMO UNA      OLA DE       FUEGO Y DE      CARICIAS  " to "Letra de una canción",
+            "  DESCUBREN      FOSFORO     EN SATURNO                 " to "Descubrimiento científico"
         ),
 
         3 to listOf(
-            "LOS SERES VIVOS CAMBIAN CON EL TIEMPO" to "Descubrimiento científico",
-            "" to "Descubrimiento científico",
+            "  LOS SERES       VIVOS      CAMBIAN CON    EL TIEMPO   " to "Descubrimiento científico",
+            "    HALLAN       PLANETAS     SIMILARES     A LA TIERRA " to "Descubrimiento científico",
             " VEO EN TI LO  QUE VAN GOGH  VEIA EN LAS    ESTRELLAS   " to "Panel del amor/desamor",
-            "ERES MI MEDIA NARANJA" to "Panel del amor/desamor",
+            "   ERES MI        MEDIA        NARANJA                  " to "Panel del amor/desamor",
             " TU ROSTRO ES LO QUE QUIERO  VER AL FINAL    DEL DIA    " to "Panel del amor/desamor"
         ),
 
         4 to listOf(
             "  TARTA DE      FRESAS Y       NATA CON     CHOCOLATE   " to "Panel con bote",
-            "" to "Panel con bote",
-            "" to "Panel con bote"
+            "  VIEIRAS AL    HORNO CON     TARTAR DE      AGUACATE   " to "Panel con bote",
+            " PASTA RELLENA  DE DATILES Y  CHAMPIÑONES               " to "Panel con bote"
         )
     )
 
@@ -41,10 +41,10 @@ class FrasesPanel {
         "A MAL TIEMPO BUENA CARA" to "Refrán español",
         "CREA FAMA Y ECHATE A DORMIR" to "Refrán español",
         "LA ALHAMBRA Y GRANADA" to "Monumento y ciudad",
-        (""),
-        (""),
-        (""),
-        (""),
-        ("")
+        "LA CELESTINA Y FERNANDO DE ROJAS" to "Libro y autor",
+        "DON JUAN TENORIO Y JOSE ZORRILLA" to "Libro y autor",
+        "BRILLANTE LUMINOSO Y RESPLANDECIENTE" to "Tres sinónimos",
+        "HERMOSO BELLO Y PRECIOSO" to "Tres sinónimos",
+        "LA SAGRADA FAMILIA Y BARCELONA" to "Monumento y ciudad"
     )
 }
--- app/src/main/java/com/example/laruletadelasuerte/PanelFinalActivity.kt
+++ app/src/main/java/com/example/laruletadelasuerte/PanelFinalActivity.kt
@@ -1,18 +1,51 @@
 package com.example.laruletadelasuerte
 
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.GridLayout
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 
 class PanelFinalActivity : AppCompatActivity() {
+
+    lateinit var frase: String
+    private lateinit var panel: GridLayout
+    private lateinit var viewModel: PanelViewModel
+    private lateinit var nombreJugador: TextView
+    private lateinit var dineroJugador: TextView
+    private lateinit var personajeJugador: ImageView
+    private lateinit var jugadores: List<Jugador>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_panel_final)
 
+        val bundle = intent.extras
+        jugadores = bundle?.getParcelableArrayList("jugadores")!!
 
+        val jugadorConMasDinero = viewModel.jugadores?.maxByOrNull { it.dineroTotal }
+        nombreJugador = findViewById(R.id.nombreJugador)
+        personajeJugador = findViewById(R.id.ivJugador)
+        dineroJugador = findViewById(R.id.dineroTotalJugador)
+
+        if (jugadorConMasDinero != null) {
+            nombreJugador.text = jugadorConMasDinero.nombre
+            personajeJugador.setImageResource(jugadorConMasDinero.imagenResId)
+            dineroJugador.text = jugadorConMasDinero.dineroTotal.toString()
+        }
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frameLayout2, RuletaFragment())
+            .commit()
 
     }
+
+
 }
